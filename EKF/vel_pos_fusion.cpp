4648462883702bd36fc10d1507dda823127969b9
Jake Dahl <jacob.dahl@tealdrones.com>
2018-08-09 10:00:47 -0600

    dfg

--- EKF/vel_pos_fusion.cpp
+++ EKF/vel_pos_fusion.cpp
@@ -163,7 +163,7 @@ void Ekf::fuseVelPosHeight()
 		// Log all this junk for development testing
 		static orb_advert_t _jake_debug_topic = nullptr;
 		jake_debug_s report = {};
-		report.z_est = ekf2_z_est;
+		report.ekf2_z_est = _state.pos(2);
 
 		report.raw_baro = _baro_sample_delayed.hgt;
 		report.baro = baro_measurement;
